8ed638e27d2e684b8adc97cca492814bb3ff47da
Hubert Wdowiak <hubert.wdowiak98@onet.pl>
2020-04-26 13:44:54 +0200

    Added randomly chosen plots for a view, Modified "make_plots" function -> now function not only creates, a plot of chosen coin, but also returns a path to the plot image

--- website/crypto/views.py
+++ website/crypto/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
-from utilities import make_plots
+from utilities import make_plots, get_random_id
 
 
 def index(request):
-    make_plots.last_month("btc-bitcoin")
-    make_plots.last_month("miota-iota")
+    coin1_id = get_random_id.get_random_id()
+    coin2_id = get_random_id.get_random_id()
+
+    path1 = make_plots.last_month(coin1_id)
+    path2 = make_plots.last_month(coin2_id)
 
     template = loader.get_template('home.html')
-    context = {"img1_name": "Last month Bitcoin pricing", "img2_name": "Last month IOTA pricing"}
+    context = {"img1_name": f"Last month {coin1_id} pricing", "img2_name": f"Last month {coin2_id} pricing",
+               "plot_path1": path1, "plot_path2": path2}
     return HttpResponse(template.render(context, request))
--- website/utilities/make_plots.py
+++ website/utilities/make_plots.py
@@ -1,31 +1,34 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import glob
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 from paprika_client.client import *
 
 
-def last_month(coin_id: str) -> None:
+def last_month(coin_id: str) -> str:
     """
     Fetch data of specified coin_id from last month (if not fetched)
     Create visualisations of those data and save to coin_id_last_month.png in crypto/static/img dir
+    Return path to a created plot
     """
     now = datetime.datetime.now()
-    month_ealier = datetime.datetime(year=now.year, month=now.month-1, day=now.day)
+    month_ealier = datetime.datetime(year=now.year, month=now.month - 1, day=now.day)
     filenames = glob.glob("data/*.json")
     needle = "{}_from_{}_to_{}.json".format(coin_id, month_ealier.strftime("%Y-%m-%d"), now.strftime("%Y-%m-%d"))
     needle = '/'.join(('data', needle))
-    if needle in filenames:
-        return
+    if needle not in filenames:
+        client = Client()
+        client.interval = "1h"
 
-    client = Client()
-    client.interval = "1h"
+        client.coin_history(coin_id, month_ealier.strftime("%Y-%m-%d"))
+        tickers = pd.read_json(needle)
+        plt.style.use("seaborn-whitegrid")
+        plt.figure(figsize=(16, 8))
+        sns.lineplot(tickers['timestamp'], tickers['price'])
+        plt.ylabel("Price [USD]")
+        plt.xlabel("Date")
+        plt.savefig("crypto/static/img/{}_last_month.png".format(coin_id))
 
-    client.coin_history(coin_id,  month_ealier.strftime("%Y-%m-%d"))
-    tickers = pd.read_json(needle)
-    plt.style.use("seaborn-whitegrid")
-    plt.figure(figsize=(16, 8))
-    sns.lineplot(tickers['timestamp'], tickers['price'])
-    plt.ylabel("Price [USD]")
-    plt.xlabel("Date")
-    plt.savefig("crypto/static/img/{}_last_month.png".format(coin_id))
+    return "static/img/{}_last_month.png".format(coin_id)

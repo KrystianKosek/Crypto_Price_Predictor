37c245570b702dc7a4d7da29681622ccd4b7672b
Krystian Kosek <krystian.kosek17@gmail.com>
2020-05-10 14:31:23 +0200

    Subpage v1.0

--- website/coin/views.py
+++ website/coin/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
 from utilities import update_data
-from .models import CoinForTable
-
+from .models import CoinForTable, Coin
 
 def index(request, coin_name):
     template = loader.get_template('coin_details.html')
-    coin_id = CoinForTable.objects.get(name=coin_name).coin_id
-    update_data.update_coin(coin_id)
+    coin = CoinForTable.objects.get(name=coin_name)
+    update_data.update_coin(coin.coin_id)
     context = {}
+
+    query_set = Coin.objects.filter(coin_id=coin.coin_id)
+    labels = []
+    data = []
+    for x in query_set:
+        labels.append(str(x.datetime_stamp.date()))
+        data.append(str(x.price))
+
+    context.update({"coin_id": coin_name, "labels": labels, "data": data})
+
     return HttpResponse(template.render(context, request))
 
 

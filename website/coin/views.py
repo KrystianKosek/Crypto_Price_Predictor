13e9d6bbd621cceb29c800bd87e7c9b1e62f108d
Hubert Wdowiak <hubert.wdowiak98@onet.pl>
2020-05-29 14:02:46 +0200

    Predict views

--- website/coin/views.py
+++ website/coin/views.py
@@ -1,8 +1,10 @@
+from crypto.models import Currency
 from django.http import HttpResponse
 from django.template import loader
+from prediction.sarimax import predict
 from utilities import update_data
+
 from .models import CoinForTable, Coin
-from crypto.models import Currency
 
 
 def index(request, coin_name):
@@ -18,49 +20,42 @@ def index(request, coin_name):
         labels.append(str(x.datetime_stamp.date()))
         data.append(str(x.price))
 
-    pln_course = Currency.objects.filter(currency_name = "PLN")[0]
+    pln_course = Currency.objects.filter(currency_name="PLN")[0]
     context.update({"coin_id": coin_name, "labels": labels, "data": data})
-    context.update({"price_USD": coin.price, "price_PLN": coin.price * pln_course.rate, "percent_change_24h": coin.percent_change_24h, "percent_change_7d": coin.percent_change_7d,
-                    "percent_change_30d": coin.percent_change_30d, "beta_value": coin.beta_value, "circulating_supply": coin.circulating_supply,
-                    "volume_24h": coin.volume_24h, "change_24h": coin.change_24h,
-                    "market_cap": coin.market_cap, "coin_ranking": int(coin.coin_ranking),"coin_name": coin.name})
+    context.update({"price_USD": coin.price, "price_PLN": coin.price * pln_course.rate,
+                    "percent_change_24h": coin.percent_change_24h, "percent_change_7d": coin.percent_change_7d,
+                    "percent_change_30d": coin.percent_change_30d, "beta_value": coin.beta_value,
+                    "circulating_supply": coin.circulating_supply, "volume_24h": coin.volume_24h,
+                    "change_24h": coin.change_24h, "market_cap": coin.market_cap,
+                    "coin_ranking": int(coin.coin_ranking), "coin_name": coin.name})
+
     if coin.max_supply == 0.0:
         context.update({"max_supply": "undefined"})
     else:
         context.update({"max_supply": coin.max_supply})
 
-    if coin.percent_change_24h < 0:
-        context.update({"24h_color": "red"})
-    elif coin.percent_change_24h > 0:
-        context.update({"24h_color": "green"})
-    else:
-        context.update({"24h_color": "black"})
+    cases = {coin.percent_change_24h: "24h_color",
+             coin.percent_change_7d: "7d_color",
+             coin.percent_change_30d: "30d_color",
+             coin.change_24h: "change_color"}
 
-    if coin.percent_change_7d < 0:
-        context.update({"7d_color": "red"})
-    elif coin.percent_change_7d > 0:
-        context.update({"7d_color": "green"})
-    else:
-        context.update({"7d_color": "black"})
-
-    if coin.percent_change_30d < 0:
-        context.update({"30d_color": "red"})
-    elif coin.percent_change_30d > 0:
-        context.update({"30d_color": "green"})
-    else:
-        context.update({"30d_color": "black"})
-
-    if coin.change_24h < 0:
-        context.update({"change_color": "red"})
-    elif coin.change_24h > 0:
-        context.update({"change_color": "green"})
-    else:
-        context.update({"change_color": "black"})
+    for case in cases:
+        if case < 0:
+            context.update({cases[case]: "red"})
+        elif case > 0:
+            context.update({cases[case]: "green"})
+        else:
+            context.update({cases[case]: "black"})
 
     return HttpResponse(template.render(context, request))
 
 
 def predict(request, coin_name):
-    return HttpResponse(coin_name)
-
-
+    template = loader.get_template('coin_details.html')
+    df = predict(coin_name)
+    context = {}
+    context.update({"prediction_index": df.index.values,
+                    "lower_pred_price": df['lower price'].values,
+                    "upper_pred_price": df['upper price'].values
+                    })
+    return HttpResponse(template.render(context, request))
